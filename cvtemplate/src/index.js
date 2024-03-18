f214801700b459152ff7a7f00670ae88f3568c8b
BuildTools <unconfigured@null.spigotmc.org>
2024-03-18 13:10:36 +0200

    curs1final

--- cvtemplate/src/index.js
+++ cvtemplate/src/index.js
@@ -3,3 +3,10 @@ import './scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+import particles from 'particles.js'
+import './js/particles.js'
+
+import '../node_modules/typed.js/dist/typed.umd.js'
+
+import './js/main.js';
